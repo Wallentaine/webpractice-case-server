7f6b764d77f3bd7ad31b1aa24bfe0472940cb1b9
Aleksey <Wallentaine@gmail.com>
2022-10-22 23:38:46 +0300

    Добавил мидлваресы

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,11 +1,12 @@
 const Router = require('express')
 const router = new Router()
 const postController = require('../controllers/postController')
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 
-router.post('/', postController.create)
+router.post('/', checkRoleMiddleware("WORKER"), postController.create)
 router.get('/latest/', postController.getLatestPosts)
 router.get('/:id', postController.getOne)
 router.get('/', postController.getAll)
-router.put('/', postController.update)
+router.put('/', checkRoleMiddleware("ADMIN"), postController.update)
 
 module.exports = router
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,12 +2,13 @@ const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userController')
 const authMiddleware = require('../middleware/authMiddleware')
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
 router.put('/image', userController.addImage)
 router.get('/auth', authMiddleware, userController.check)
 router.get('/email', userController.getUserByEmail)
-router.get('/', userController.getAll)
+router.get('/', checkRoleMiddleware("ADMIN"), userController.getAll)
 
 module.exports = router
