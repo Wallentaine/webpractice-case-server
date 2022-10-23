79e855c4b6efc00fcf768299f823a4748693ba14
Aleksey <Wallentaine@gmail.com>
2022-10-23 13:41:23 +0300

    heroku

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -3,7 +3,7 @@ const router = new Router()
 const postController = require('../controllers/postController')
 const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 
-router.post('/', checkRoleMiddleware("WORKER"), postController.create)
+router.post('/', postController.create)
 router.get('/latest/', postController.getLatestPosts)
 router.get('/:id', postController.getOne)
 router.get('/', postController.getAll)
